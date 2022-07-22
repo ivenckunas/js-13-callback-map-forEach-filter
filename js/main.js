6cd7c97364b263f0c9e7b6326eea4ebb1ca879c1
ivenckunas <ignas.venckunas@gmail.com>
2022-07-22 14:03:32 +0300

    js-13-callback

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function displayAsH2(str) {
+  const h2 = document.getElementById("h2");
+  h2.textContent = str;
+}
+
+function displayAsParagraph(value) {
+  const paraEl = document.getElementById("para");
+  paraEl.textContent = `The value is ${value}!`;
+}
+
+displayAsH2("hello world");
+
+function myPower(num, callbackfn) {
+  const result = num ** 3;
+  callbackfn(result);
+}
+
+myPower(3, displayAsParagraph);
+myPower(32, displayAsH2);
+
+// displayAsParagraph(result);
